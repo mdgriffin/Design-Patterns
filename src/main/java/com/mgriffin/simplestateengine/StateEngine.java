a4f891dcc8fdbee08b398f6e2a19080f5e496d49
Michael Griffin <mdgriffin064@gmail.com>
2018-09-16 22:14:27 +0100

    Added current start property, setting it to the start state in the constructor

--- src/main/java/com/mgriffin/simplestateengine/StateEngine.java
+++ src/main/java/com/mgriffin/simplestateengine/StateEngine.java
@@ -1,7 +1,9 @@
 package com.mgriffin.simplestateengine;
 
 import java.util.EnumSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class StateEngine {
 
@@ -9,14 +11,28 @@ public class StateEngine {
 
     private EnumSet events;
 
-    private Set<Enum> transitions;
+    private Enum currentState;
 
-    public StateEngine (EnumSet states, EnumSet events) {
+    private Map<Enum, Map<Enum, Enum>> transitions;
+
+    public StateEngine (EnumSet states, EnumSet events, Enum startState) {
+        if (!states.contains(startState)) {
+            throw new IllegalArgumentException("Start State must be one of the defined states");
+        }
+
         this.states = states;
         this.events = events;
+        this.currentState = startState;
+        transitions = new HashMap();
+        Iterator<Enum> statesIterator = states.iterator();
+
+        while(statesIterator.hasNext()) {
+            transitions.put(statesIterator.next(), new HashMap());
+        }
     }
 
     public void addTransition (Enum startState, Enum endState, Enum event) {
-
+        transitions.get(startState).put(event, endState);
     }
+
 }
--- src/test/java/com/mgriffin/simplestateengine/StateEngineTest.java
+++ src/test/java/com/mgriffin/simplestateengine/StateEngineTest.java
@@ -1,6 +1,7 @@
 package com.mgriffin.simplestateengine;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.EnumSet;
 
@@ -8,23 +9,31 @@ import static junit.framework.TestCase.fail;
 
 public class StateEngineTest {
 
-    private States states;
-    private Events events;
 
     @Test
     public void stateEngineShouldInstantiate () {
-        StateEngine se = new StateEngine(EnumSet.allOf(States.class), EnumSet.allOf(Events.class));
+        StateEngine se = new StateEngine(EnumSet.allOf(States.class), EnumSet.allOf(Events.class), States.ALIVE);
     }
 
     @Test
     public void whenAddingATransition_noExceptionThrown () {
-        StateEngine se = new StateEngine(EnumSet.allOf(States.class), EnumSet.allOf(Events.class));
+        StateEngine se = new StateEngine(EnumSet.allOf(States.class), EnumSet.allOf(Events.class), States.ALIVE);
         se.addTransition(States.ALIVE, States.DEAD, Events.SHOOT);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenAddingInvalidStartState_exceptionThrown() {
+        StateEngine se = new StateEngine(EnumSet.allOf(States.class), EnumSet.allOf(Events.class), OtherStates.OPEN);
     }
 
     private enum States {
         ALIVE,
         DEAD
+    }
+
+    private enum OtherStates {
+        OPEN,
+        CLOSED
     }
 
     private enum Events {
