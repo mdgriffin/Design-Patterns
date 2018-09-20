00297bf847ec3fc145390587fc048881bc201d8b
Michael Griffin <mdgriffin064@gmail.com>
2018-09-20 22:32:17 +0100

    Added lifecycle methods to coffee machine

--- src/main/java/com/mgriffin/coffemat/OrderService.java
+++ src/main/java/com/mgriffin/coffemat/OrderService.java
@@ -1,5 +1,6 @@
 package com.mgriffin.coffemat;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,11 +10,19 @@ public class OrderService {
 
     private List<CoffeeOrder> orders = new LinkedList<>();
 
-    public OrderService (List<CoffeeMachine> coffeeMachines) {
-        this.coffeeMachines = coffeeMachines;
+    public OrderService (int numMachines) {
+        this.coffeeMachines = new ArrayList<>();
+
+        for (int i = 0; i < numMachines; i++) {
+            this.coffeeMachines.add(new CoffeeMachine(this));
+        }
     }
 
     public void addOrder(CoffeeOrder order) {
         orders.add(order);
+    }
+
+    public void orderCompleted (CoffeeMachine coffeeMachine, CoffeeOrder coffeeOrder) {
+
     }
 }
