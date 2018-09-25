778e19305a1c31da21b771a7641cc670c83bd4ad
Michael Griffin <mdgriffin064@gmail.com>
2018-09-25 20:41:59 +0100

    Starting an available coffee machine if there is one available

--- src/main/java/com/mgriffin/coffemat/OrderService.java
+++ src/main/java/com/mgriffin/coffemat/OrderService.java
@@ -3,12 +3,13 @@ package com.mgriffin.coffemat;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 public class OrderService {
 
     private List<CoffeeMachine> coffeeMachines;
 
-    private List<CoffeeOrder> orders = new LinkedList<>();
+    private LinkedList<CoffeeOrder> orders = new LinkedList<>();
 
     public OrderService (int numMachines) {
         this.coffeeMachines = new ArrayList<>();
@@ -20,9 +21,24 @@ public class OrderService {
 
     public void addOrder(CoffeeOrder order) {
         orders.add(order);
+
+        Optional<CoffeeMachine> availableMachine = getAvailableCoffeeMachine();
+
+        if (availableMachine.isPresent()) {
+            availableMachine.get().start(order);
+        }
+    }
+
+
+    private Optional<CoffeeMachine> getAvailableCoffeeMachine () {
+        return coffeeMachines.stream().filter(coffeeMachine -> coffeeMachine.available()).findFirst();
     }
 
     public void orderCompleted (CoffeeMachine coffeeMachine, CoffeeOrder coffeeOrder) {
+        orders.remove(coffeeOrder);
 
+        if (orders.size() > 0) {
+            coffeeMachine.start(orders.getFirst());
+        }
     }
 }
