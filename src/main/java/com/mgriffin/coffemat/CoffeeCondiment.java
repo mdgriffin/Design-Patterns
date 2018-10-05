3a4ec83b349875a073c4afc736c0c4ad77817aab
Michael Griffin <mdgriffin064@gmail.com>
2018-10-05 14:12:43 +0100

    Added method for adding condiments to order

--- src/main/java/com/mgriffin/coffemat/CoffeeCondiment.java
+++ src/main/java/com/mgriffin/coffemat/CoffeeCondiment.java
@@ -1,12 +1,20 @@
 package com.mgriffin.coffemat;
 
 public enum CoffeeCondiment implements Billable {
-    CREAM (0.2d);
+    CREAM ("Cream", 0.2d),
+    MILK("Milk", 0.1d),
+    SUGAR("Sugar", 0d);
 
+    private String displayName;
     private double price;
 
-    CoffeeCondiment (double price) {
+    CoffeeCondiment (String displayName, double price) {
+        this.displayName = displayName;
         this.price = price;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     @Override
--- src/main/java/com/mgriffin/driver/ConsoleDriver.java
+++ src/main/java/com/mgriffin/driver/ConsoleDriver.java
@@ -10,6 +10,7 @@ public class ConsoleDriver {
 
     public static void main (String[] args) {
         List<CoffeeMachine> coffeeMachines = new ArrayList<>();
+        CoffeeOrder.CoffeeOrderBuilder coffeeOrderBuilder = new CoffeeOrder.CoffeeOrderBuilder();
 
         for (int i = 0; i < 4; i++) {
             coffeeMachines.add(new CoffeeMachineImpl());
@@ -17,18 +18,12 @@ public class ConsoleDriver {
 
         OrderService orderService = new OrderServiceImpl(coffeeMachines);
 
-        Customer customer = getCustomer();
-        CoffeeType coffeeType = getCoffeeType();
-        CoffeeSize coffeeSize = getSize();
+        coffeeOrderBuilder.setCustomer(getCustomer());
+        coffeeOrderBuilder.setType(getCoffeeType());
+        coffeeOrderBuilder.setSize(getSize());
+        getCondiments().stream().forEach(coffeeCondiment -> coffeeOrderBuilder.addCondiment(coffeeCondiment));
 
-        CoffeeOrder order = new CoffeeOrder.CoffeeOrderBuilder()
-            .setCustomer(customer)
-            .setType(coffeeType)
-            .setSize(coffeeSize)
-            .addCondiment(CoffeeCondiment.CREAM)
-            .order();
-
-        orderService.addOrder(order);
+        orderService.addOrder(coffeeOrderBuilder.order());
 
         System.out.println(orderService.toString());
     }
@@ -73,6 +68,45 @@ public class ConsoleDriver {
         }
 
         return CoffeeSize.values()[option - 1];
+    }
+
+    private static List<CoffeeCondiment> getCondiments () {
+        List<CoffeeCondiment> condiments = new ArrayList<>();
+
+        condiments.add(getCondiment());
+
+        System.out.println("Would you like add another condiment, (Y)es, (N)o?:");
+        String choice = scanner.nextLine();
+
+        while (choice.length() > 0 && choice.toLowerCase().charAt(0) == 'y') {
+            condiments.add(getCondiment());
+            System.out.println("Would you like add another condiment, (Y)es, (N)o?:");
+            choice = scanner.nextLine();
+        }
+
+        return condiments;
+    }
+
+    private static CoffeeCondiment getCondiment () {
+        int option = -1;
+        System.out.println("Please select condiments: ");
+        Arrays.stream(CoffeeCondiment.values()).forEach(size -> System.out.println((size.ordinal() + 1) + ": " + size.getDisplayName()));
+
+        while (option <= 0 || option > CoffeeCondiment.values().length) {
+            try {
+                option = scanner.nextInt();
+                scanner.nextLine();
+
+                if (option <= 0 || option > CoffeeCondiment.values().length) {
+                    System.out.println("Invalid option please select again: ");
+                }
+            } catch(InputMismatchException exc) {
+                scanner.nextLine();
+                System.out.println("Invalid option, please enter a number: ");
+            }
+        }
+
+        return CoffeeCondiment.values()[option - 1];
     }
 
     private static Customer getCustomer () {
