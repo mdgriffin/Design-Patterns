157f11e79f004c34a2ec3d278b7935ba547d70f4
Michael Griffin <mdgriffin064@gmail.com>
2018-11-28 16:45:29 +0000

    Added tests for coffee receipt printer

---
+++ src/test/java/com/mgriffin/billing/CoffeeExpressReceiptPrinterTest.java
@@ -0,0 +1,36 @@
+package com.mgriffin.billing;
+
+import com.mgriffin.order.*;
+import com.mgriffin.utility.StringUtils;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static com.mgriffin.utility.StringUtils.lineBreak;
+import static com.mgriffin.utility.StringUtils.lineBreakAfter;
+import static com.mgriffin.utility.StringUtils.join;
+
+public class CoffeeExpressReceiptPrinterTest {
+
+    @Test
+    public void receiptPrinter_canPrint () {
+        CoffeeOrder coffeeOrder = new CoffeeOrder.CoffeeOrderBuilder()
+            .setDiscount((amount) -> { return amount * 1d;})
+            .setCustomer(new Customer("John Doe"))
+            .setType(CoffeeType.LATTE)
+            .setSize(CoffeeSize.LARGE)
+            .addCondiment(CoffeeCondiment.CREAM)
+            .order();
+
+        ReceiptPrinter receiptPrinter = new CoffeeExpressReceiptPrinter(coffeeOrder);
+
+        String res = receiptPrinter.print();
+
+        assertEquals(res, lineBreak(join(
+        lineBreakAfter("=== Your Order ==="),
+            lineBreakAfter("LARGE LATTE with Cream,"),
+            lineBreakAfter("Price: 3.7"),
+            lineBreakAfter(""),
+            "Thanks For Your Custom")));
+    }
+
+}
